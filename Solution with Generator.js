c43d68463321dad3ac668cea1476220e80fb08c5
Lachezar Kutsarov <lkutsarov@gmail.com>
2022-05-11 07:30:56 +0200

    Update Solution with Generator.js

--- Solution with Generator.js
+++ Solution with Generator.js
@@ -1,62 +1,80 @@
 
-#include <iterator>
-#include <utility>
-#include <deque>
-#include <vector>
-using namespace std;
+/**
+ * @constructor
+ * @param {NestedInteger[]} nestedList
+ */
+var NestedIterator = function (nestedList) {
+    this.nextElement = null;
+    this.nestedGenerator = this.nestedElementGenerator(nestedList);
+};
+
+/**
+ * @param {NestedInteger[]} nestedList
+ * @yield {NestedInteger}
+ */
+NestedIterator.prototype.nestedElementGenerator = function* (nestedList) {
+    for (let nestedElement of nestedList) {
+        if (nestedElement.isInteger()) {
+            yield nestedElement.getInteger();
+        } else {
+            yield * this.nestedElementGenerator(nestedElement.getList());
+        }
+    }
+};
+
+/**
+ * @this NestedIterator
+ * @return {NestedInteger}
+ */
+NestedIterator.prototype.next = function () {
+    if (!this.hasNext()) {
+        throw "Element not found.";
+    }
+
+    let currentValue = this.nextElement;
+    this.nextElement = null;
+    return currentValue;
+};
+
+/**
+ * @this NestedIterator
+ * @returns {boolean}
+ */
+NestedIterator.prototype.hasNext = function () {
+    if (this.nextElement !== null) {
+        return true;
+    }
+    const {value, done} = this.nestedGenerator.next();
+    if (done) {
+        return false;
+    }
+    this.nextElement = value;
+    return true;
+};
 
 /*
  Interface NestedInteger is in-built in the solution file on leetcode.com. 
  When running the code on the website, do not include this interface.
  */
-class NestedInteger {
-public:
-    // Return true if this NestedInteger holds a single integer, rather than a nested list.
-    bool isInteger() const;
+function NestedInteger() {
 
-    // Return the single integer that this NestedInteger holds, if it holds a single integer
-    // The result is undefined if this NestedInteger holds a nested list
-    int getInteger() const;
+    //  Return true if this NestedInteger holds a single integer, rather than a nested list.
+    //  @return {boolean}
+    this.isInteger = function () {
+        // ...
+    };
+
+    //  Return the single integer that this NestedInteger holds, if it holds a single integer
+    //   Return null if this NestedInteger holds a nested list
+    // @return {integer}
+    this.getInteger = function () {
+        // ...
+    };
 
     // Return the nested list that this NestedInteger holds, if it holds a nested list
-    // The result is undefined if this NestedInteger holds a single integer
-    const vector<NestedInteger>& getList() const;
-};
-
-class NestedIterator {
-    
-    typedef vector<NestedInteger>::const_iterator nestedIterator;
-    deque<pair<nestedIterator, nestedIterator>> stack;
-
-public:
-    NestedIterator(vector<NestedInteger>& nestedList) {
-        stack.emplace_front(nestedList.begin(), nestedList.end());
-    }
-
-    int next() {
-        if (!hasNext()) {
-            throw out_of_range("Element not found.");
-        }
-
-        int next = (*stack.front().first).getInteger();
-        ++stack.front().first;
-        return next;
-    }
-
-    bool hasNext() {
-        while (!stack.empty()) {
-            if (stack.front().first == stack.front().second) {
-                stack.pop_front();
-                continue;
-            }
-            if ((*stack.front().first).isInteger()) {
-                return true;
-            }
-            nestedIterator begin = (*stack.front().first).getList().begin();
-            nestedIterator end = (*stack.front().first).getList().end();
-            ++stack.front().first;
-            stack.emplace_front(begin, end);
-        }
-        return false;
-    }
-};
+    // Return null if this NestedInteger holds a single integer
+    //  @return {NestedInteger[]}
+    this.getList = function () {
+        // ...
+    };
+}
